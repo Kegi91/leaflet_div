d46652cca2486e789743577f617f5aed4ca5fde9
Kuisma Juho K <kujuku@dx7-biophys-04.physics.helsinki.fi>
2017-07-17 11:08:02 +0300

    Universality

--- leaflet_div.py
+++ leaflet_div.py
@@ -1,37 +1,43 @@
 import numpy as np
 
-def get_num(x):
-    return int(''.join(c for c in x if c.isdigit()))
+def skip_lines(f, i):
+    for i in range(i):
+        next(f)
 
-def print_leaflet(fname, center):
+def read_gro_line(f_in):
+    l = f_in.readline()
+    if l == '' or len(l)<45:
+        return ''
+
+    # a_number = int(l[15:20])
+    # x = float(l[20:28])
+    # y = float(l[28:36])
+
+    res_name = l[5:10].strip()
+    res_num = int(l[0:5])
+    a_name = l[10:15].strip()
+    z = float(l[36:44])
+
+    return res_name, res_num, a_name, z
+
+def print_leaflet(fname, center, res_name, atom_name):
     f = open(fname, 'r')
-    next(f)
-    next(f)
-
-    line = f.readline()
-    split = line.split()
+    skip_lines(f,2)
 
     upper = []
     lower = []
 
-    while line != '':
-        if len(split) not in [5,6]: # TODO: Make universal
-            line = f.readline()
-            split = line.split()
-            continue
+    while True:
+        try:
+            res, res_num, atom, z = read_gro_line(f)
+        except ValueError:
+            break
 
-        res = split[0]
-        atom = split[1]
-        z = float(split[-1]) # TODO: Make universal
-
-        if res.find("DPPC") != -1 and atom.find("P") != -1: # TODO: Make universal
+        if res.find(res_name) != -1 and atom.find(atom_name) != -1:
             if z < center:
-                lower.append(get_num(res))
+                lower.append(res_num)
             else:
-                upper.append(get_num(res))
-
-        line = f.readline()
-        split = line.split()
+                upper.append(res_num)
 
     f.close()
 
@@ -40,36 +46,22 @@ def print_leaflet(fname, center):
     print("\nLower:\t%d-%d"%(lower[0],lower[-1]))
     print_consecutive_elems(lower)
 
-def find_center(fname):
+def find_center(fname, res_name, atom_name):
     f = open(fname, 'r')
-    next(f)
-    next(f)
-
-    line = f.readline()
-    split = line.split()
-
-    upper = []
-    lower = []
+    skip_lines(f,2)
 
     z_sum = 0
     z_numb = 0
 
-    while line != '':
-        if len(split) not in [5,6]: # TODO
-            line = f.readline()
-            split = line.split()
-            continue
+    while True:
+        try:
+            res, res_num, atom, z = read_gro_line(f)
+        except ValueError:
+            break
 
-        res = split[0]
-        atom = split[1]
-        z = float(split[-1]) # TODO
-
-        if res.find("DPPC") != -1 and atom.find("P") != -1: # TODO
+        if res.find(res_name) != -1 and atom.find(atom_name) != -1:
             z_sum += z
             z_numb += 1
-
-        line = f.readline()
-        split = line.split()
 
     f.close()
     return z_sum/z_numb
@@ -77,6 +69,7 @@ def find_center(fname):
 def print_consecutive_elems(array):
     prev = array[0]
     low = array[0]
+
     for i in range(1,len(array)):
         if array[i] == prev + 1:
             prev = array[i]
@@ -90,5 +83,7 @@ def print_consecutive_elems(array):
 
 print_leaflet(
     "input/system_solv.gro",
-    find_center("input/system_solv.gro")
+    find_center("input/system_solv.gro", "DPPC", "P"),
+    "DPPC",
+    "P"
 )
